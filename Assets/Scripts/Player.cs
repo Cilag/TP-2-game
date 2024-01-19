603e02dc7b9612357037d48bdbd8b4d89a64ea38
OZOUX Guillaume <112950617+Cilag@users.noreply.github.com>
2024-01-19 13:37:20 +0100

    fini

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform GFX;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform feetPos;
+    [SerializeField] private float groundDistance = 0.25f;
+    [SerializeField] private float jumpTime = 0.3f;
+
+    [SerializeField] private float crouchHeight = 0.5f;
+
+    private bool isGrounded = false;
+    private bool isJumping = false;
+    private float jumpTimer;
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, groundDistance, groundLayer);
+        if(isGrounded && Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        if(isJumping && Input.GetButton("Jump"))
+        {
+            if (jumpTimer < jumpTime)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+
+                jumpTimer += Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if(Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+            jumpTimer = 0;
+        }
+    
+        if (isGrounded && Input.GetButton("Crouch"))
+        {
+            GFX.localScale = new Vector3(GFX.localScale.x, crouchHeight, GFX.localScale.z);
+        }
+
+        if (isJumping)
+        {
+            GFX.localScale = new Vector3(GFX.localScale.x, 1f, GFX.localScale.z);
+        }
+
+        if (Input.GetButton("Crouch"))
+        {
+            GFX.localScale = new Vector3(GFX.localScale.x, 1f, GFX.localScale.z);
+        }
+    }
+}
---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UiManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreUI;
+
+    [SerializeField] private GameObject startMenuUI;
+
+    GameManager gm;
+
+    private void Start()
+    {
+        gm = GameManager.Instance;
+        gm.onGameOver.AddListener(ActivateGameOverUI);
+    }
+    
+    public void PlayButtonHandler()
+    {
+        gm.StartGame();
+        
+    }
+
+    public void ActivateGameOverUI()
+    {
+        startMenuUI.SetActive(true);
+    }
+    
+    private void OnGUI()
+    {
+        scoreUI.text = gm.PrettyScore();
+    }
+}
